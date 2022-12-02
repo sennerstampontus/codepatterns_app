a239737a74441c7325732f4f2b2716ee6c9992d8
Pontus Sennerstam <pontus.sennerstam@learnet.se>
2022-12-02 09:37:35 +0100

    :sparkles: Add animation to Shop Now Button

--- src/components/items/ShopButtonComponent.vue
+++ src/components/items/ShopButtonComponent.vue
@@ -1,5 +1,9 @@
 <template>
-     <div class="button-container">
+     <div
+          class="button-container"
+          @mouseover="animateIn"
+          @mouseout="animateOut"
+     >
           <div class="left-bottom-border"></div>
           <div class="left-top-border"></div>
           <div class="right-bottom-border"></div>
@@ -10,7 +14,92 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import gsap from 'gsap';
 export default defineComponent({
      name: 'ShopButtonComponent',
+     methods: {
+          animateIn() {
+               const tl = gsap.timeline();
+               tl.add('start')
+                    .to(
+                         '.left-bottom-border',
+                         {
+                              left: 30,
+                         },
+                         'start'
+                    )
+                    .to(
+                         '.left-top-border',
+                         {
+                              left: 30,
+                              right: 132.7,
+                         },
+                         'start'
+                    )
+                    .to(
+                         '.right-bottom-border',
+                         {
+                              right: 30,
+                              left: 132.7,
+                         },
+                         'start'
+                    )
+                    .to(
+                         '.right-top-border',
+                         {
+                              right: 30,
+                         },
+                         'start'
+                    )
+                    .to(
+                         '.button-container',
+                         {
+                              background: '#c95b5b',
+                         },
+                         'start'
+                    );
+          },
+          animateOut() {
+               const tl = gsap.timeline();
+               tl.add('start')
+                    .to(
+                         '.left-bottom-border',
+                         {
+                              left: 10,
+                         },
+                         'start'
+                    )
+                    .to(
+                         '.left-top-border',
+                         {
+                              left: 10,
+                              right: 154.724,
+                         },
+                         'start'
+                    )
+                    .to(
+                         '.right-bottom-border',
+                         {
+                              right: 10,
+                              left: 154.724,
+                         },
+                         'start'
+                    )
+                    .to(
+                         '.right-top-border',
+                         {
+                              right: 10,
+                         },
+                         'start'
+                    )
+                    .to(
+                         '.button-container',
+                         {
+                              background: '#ff7373',
+                         },
+                         'start'
+                    );
+          },
+     },
 });
 </script>
